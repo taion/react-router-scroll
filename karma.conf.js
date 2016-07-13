0a292cc78e37821d6c86f0574843340f37f7af8c
Jimmy Jia <tesrin@gmail.com>
2016-07-13 13:25:10 -0400

    Update tooling

--- karma.conf.js
+++ karma.conf.js
@@ -1,2 +1,52 @@
-require('babel-register');
-module.exports = require('./karma.conf.babel.js');
+const webpack = require('webpack');
+
+module.exports = config => {
+  const { env } = process;
+
+  const isCi = env.CONTINUOUS_INTEGRATION === 'true';
+
+  config.set({
+    frameworks: ['mocha'],
+
+    files: ['test/index.js'],
+
+    preprocessors: {
+      'test/index.js': ['webpack', 'sourcemap'],
+    },
+
+    webpack: {
+      module: {
+        loaders: [
+          { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+        ],
+      },
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('test'),
+        }),
+      ],
+      devtool: 'cheap-module-inline-source-map',
+    },
+
+    webpackMiddleware: {
+      noInfo: true,
+    },
+
+    reporters: ['mocha'],
+
+    mochaReporter: {
+      output: 'autowatch',
+    },
+
+    customLaunchers: {
+      ChromeCi: {
+        base: 'Chrome',
+        flags: ['--no-sandbox'],
+      },
+    },
+
+    browsers: env.BROWSER ? env.BROWSER.split(',') : ['Chrome', 'Firefox'],
+
+    singleRun: isCi,
+  });
+};
